b8d1182eaf8a653512617540eb3c4a8e2dd65a72
ledetav <nord.hilman@gmail.com>
2023-02-25 22:22:16 +0300

    rewrite task_1 for FoDM on Java

--- Fundamentals of Discrete mathematics/Java/task_1.java
+++ Fundamentals of Discrete mathematics/Java/task_1.java
@@ -1,43 +1,42 @@
 import java.util.*;
 
-class SetTransform {
-    public static Set<Integer> inputSet() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            while (true) {
-                String userInput = scanner.nextLine().trim();
-                String[] inputList = userInput.split(" ");
-                try {
-                    Set<Integer> setInt = new HashSet<>();
-                    for (String s : inputList) {
-                        setInt.add(Integer.parseInt(s));
-                    }
-                    return setInt;
-                } catch (NumberFormatException e) {
-                    System.out.println("Error: set can only consist of integers");
-                }
-            }
-        }
+public class task_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read the four sets from user input
+        Set<Integer> setA = readSetFromInput(scanner, "A");
+        Set<Integer> setB = readSetFromInput(scanner, "B");
+        Set<Integer> setU = readSetFromInput(scanner, "U");
+        Set<Integer> setC = readSetFromInput(scanner, "C");
+
+        // Transform the sets according to the formula (A||B)&(U\C)
+        Set<Integer> unionAB = new HashSet<>(setA);
+        unionAB.addAll(setB);
+        Set<Integer> complementC = new HashSet<>(setU);
+        complementC.removeAll(setC);
+        Set<Integer> result = new HashSet<>(unionAB);
+        result.retainAll(complementC);
+
+        // Print the result
+        System.out.println("Result: " + result);
     }
 
-    public static void main(String[] args) {
-        System.out.println("Enter set A:");
-        Set<Integer> setA = inputSet();
-
-        System.out.println("Enter set B:");
-        Set<Integer> setB = inputSet();
-
-        System.out.println("Enter set U:");
-        Set<Integer> setU = inputSet();
-
-        System.out.println("Enter set C:");
-        Set<Integer> setC = inputSet();
-
-        Set<Integer> result = new HashSet<>(setA);
-        result.retainAll(setB);
-        Set<Integer> setUC = new HashSet<>(setU);
-        setUC.removeAll(setC);
-        result.addAll(setUC);
-
-        System.out.println("Transformed set: " + result);
+    // Helper method to read a set from user input
+    private static Set<Integer> readSetFromInput(Scanner scanner, String setName) {
+        System.out.print("Enter set " + setName + ": ");
+        String inputLine = scanner.nextLine();
+        String[] elements = inputLine.split("\\s+");
+        Set<Integer> set = new HashSet<>();
+        for (String element : elements) {
+            try {
+                int value = Integer.parseInt(element);
+                set.add(value);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: invalid input for set " + setName);
+                return readSetFromInput(scanner, setName);
+            }
+        }
+        return set;
     }
 }
